47ac3626a3cc24f100f55265145d47f1ee0cba00
Sheikh MD Sazidul Islam <sheikhmdsazidulislam@gmail.com>
2022-09-02 17:30:11 +0600

    added virtual port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require('cors');
 let users = require('./data.json');
 let user;
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors())
 app.use(express.json())
